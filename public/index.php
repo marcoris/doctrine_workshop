2bd964317967d5b125bd55b215894525d29e28cd
Marco Ris <ris@contria.com>
2022-01-18 11:37:20 +0100

    Update index.php

--- public/index.php
+++ public/index.php
@@ -6,19 +6,10 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-    <style>
-        #footer {
-            position:absolute;
-            bottom:0;
-            width:100%;
-            height:60px;   /* Height of the footer */
-            background:#6cf;
-        }
-    </style>
     <title>Doctrine Blogpost</title>
 </head>
-<body>
-<nav class="navbar navbar-dark bg-primary">
+<body class="mb-5">
+<nav class="navbar navbar-expand-lg navbar-dark bg-primary fixed-top">
     <div class="container-fluid">
         <a class="navbar-brand" href="#">Doctrine Blogpost</a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
@@ -30,40 +21,25 @@
                     <a class="nav-link active" aria-current="page" href="#">Home</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="#">Link</a>
-                </li>
-                <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                        Dropdown
-                    </a>
-                    <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-                        <li><a class="dropdown-item" href="#">Action</a></li>
-                        <li><a class="dropdown-item" href="#">Another action</a></li>
-                        <li><hr class="dropdown-divider"></li>
-                        <li><a class="dropdown-item" href="#">Something else here</a></li>
-                    </ul>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link disabled">Disabled</a>
+                    <a class="nav-link" href="#">Login</a>
                 </li>
             </ul>
             <form class="d-flex">
-                <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-                <button class="btn btn-outline-success" type="submit">Search</button>
+                <input class="form-control me-2" type="search" placeholder="<?php echo _("Search..."); ?>" aria-label="Search">
+                <button class="btn btn-primary" type="submit"><?php echo _("Search"); ?></button>
             </form>
         </div>
     </div>
 </nav>
-<div class="container-fluid">
-    <div class="row justify-content-md-center">
-        <div class="col-8">
-            <div class="jumbotron">
-                <h1 class="display-1">Willkommen beim Doctrine Blogpost</h1>
-                <p class="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
-            </div>
+<div class="d-flex justify-content-center p-3 mt-5">
+    <div class="container-sm">
+        <div class="jumbotron">
+            <h1 class="display-1"><?php echo _("Welcome to the doctrine blogpost"); ?></h1>
+            <p class="lead"><?php echo _("This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information."); ?></p>
+        </div>
         <?php
         require_once "bootstrap.php";
-        require_once "blog.php";
+        require_once "posts.php";
 
         $em = getEntityManager();
 
@@ -87,15 +63,14 @@
         ?>
         <!-- Option 1: Bootstrap Bundle with Popper -->
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
-        </div>
-        <footer class="footer bg-primary">
-            <!-- Copyright -->
-            <div class="text-center p-3 text-white">
-                © <?php echo date("Y"); ?> by Ocram
-            </div>
-            <!-- Copyright -->
-        </footer>
     </div>
+    <footer class="footer bg-primary" style="position:fixed;height:50px;bottom:0;left:0;right:0;margin-bottom:0;">
+        <!-- Copyright -->
+        <div class="text-center p-3 text-white">
+            © <?php echo date("Y"); ?> by Ocram
+        </div>
+        <!-- Copyright -->
+    </footer>
 </div>
 </body>
 </html>
