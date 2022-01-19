283523aef8751784e6f39fad0bebdf214592ead6
Marco Ris <ris@contria.com>
2022-01-19 09:32:53 +0100

    Update index.php

--- public/index.php
+++ public/index.php
@@ -32,37 +32,51 @@
     </div>
 </nav>
 <div class="d-flex justify-content-center p-3 mt-5">
-    <div class="container-sm">
-        <div class="jumbotron">
-            <h1 class="display-1"><?php echo _("Welcome to the doctrine blogpost"); ?></h1>
-            <p class="lead"><?php echo _("This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information."); ?></p>
+    <div class="container">
+        <div class='row'>
+            <div class='col-12'>
+                <div class="jumbotron">
+                    <h1 class="display-1"><?php echo _("Welcome to the doctrine blogpost"); ?></h1>
+                    <p class="lead"><?php echo _("This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information."); ?></p>
+                </div>
+            </div>
+            <div class="col-9">
+                <?php
+                require_once "bootstrap.php";
+                require_once "posts.php";
+//                require_once "post.php";
+//                require_once "author.php";
+
+                $em = getEntityManager();
+
+                //$category = new \App\Entities\Category();
+                //$category->setName("Category 1");
+                //
+                //$comment = new \App\Entities\Comment();
+                //$comment->setComment("This is my first comment");
+                //$comment->setAuthorName("Marco");
+                //$comment->setAuthorEmail("marco.ris@risdesign.ch");
+                //$comment->setAuthorUrl("risdesign.ch");
+                //
+                //$em->persist($comment);
+                //$em->flush();
+
+                //$comment = $em->getRepository(\App\Entities\Comment::class)->find(1);
+                //echo $comment->getAuthorName() . "<br>";
+                //echo $comment->getAuthorEmail() . "<br>";
+                //echo $comment->getAuthorUrl() . "<br>";
+                //echo $comment->getComment();
+                ?>
+                <!-- Option 1: Bootstrap Bundle with Popper -->
+                <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+            </div>
+            <div class="col-3 bg-light p-2">
+                <h3 class="display-6 text-center"><?php echo _('Archive'); ?></h3>
+                <ul>
+                    <li><a href="#">Januar 2022</a></li>
+                </ul>
+            </div>
         </div>
-        <?php
-        require_once "bootstrap.php";
-        require_once "posts.php";
-
-        $em = getEntityManager();
-
-        //$category = new \App\Entities\Category();
-        //$category->setName("Category 1");
-        //
-        //$comment = new \App\Entities\Comment();
-        //$comment->setComment("This is my first comment");
-        //$comment->setAuthorName("Marco");
-        //$comment->setAuthorEmail("marco.ris@risdesign.ch");
-        //$comment->setAuthorUrl("risdesign.ch");
-        //
-        //$em->persist($comment);
-        //$em->flush();
-
-        //$comment = $em->getRepository(\App\Entities\Comment::class)->find(1);
-        //echo $comment->getAuthorName() . "<br>";
-        //echo $comment->getAuthorEmail() . "<br>";
-        //echo $comment->getAuthorUrl() . "<br>";
-        //echo $comment->getComment();
-        ?>
-        <!-- Option 1: Bootstrap Bundle with Popper -->
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
     </div>
     <footer class="footer bg-primary" style="position:fixed;height:50px;bottom:0;left:0;right:0;margin-bottom:0;">
         <!-- Copyright -->
